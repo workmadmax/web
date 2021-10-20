755e10c6bf3f399728faa760828fd63d9dc2962c
workmadmax <80357524+workmadmax@users.noreply.github.com>
2021-10-20 20:59:31 -0300

    Aula02-1h21m

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,17 +1,18 @@
-import { VscGithubInverted } from 'react-icons/vsc'
-import styles from './styles.module.scss'
+import { VscGithubInverted } from "react-icons/vsc";
 
+import { useAuth } from "../../contexts/auth";
+import styles from "./styles.module.scss";
 
 export function LoginBox() {
+  const { signInUrl } = useAuth();
 
-    return (
-        <div className={styles.loginBoxWrapper}>
-
-            <strong>Entre e compartilhe sua  mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
-                <VscGithubInverted size="24"/>
-                Entrar Com Github
-            </a>
-        </div>
-    )
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signWithGithubButton}>
+        <VscGithubInverted size={24} />
+        Entrar com Github
+      </a>
+    </div>
+  );
 }
